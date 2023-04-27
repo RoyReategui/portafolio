c33ebdcbea7bef5e86a717e53faed9d5d1564a59
Roy Reategui <roy.reateguia@gmail.com>
2023-04-27 08:40:31 -0500

    Empty portafolio Project

---
+++ build/js/main.js
@@ -0,0 +1,1 @@
+const h2=document.getElementById("title"),user={name:"paty",edad:15,email:"paty@gmail.com"};console.log(puser);
---
+++ js/main.js
@@ -0,0 +1,9 @@
+const h2 = document.getElementById('title')
+
+const user = {
+    name: 'paty',
+    edad: 15,
+    email: "paty@gmail.com"
+}
+
+console.log(puser);
