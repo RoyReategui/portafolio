d4fb02be707fce0693f811414860775afd4ec9b3
Roy Reategui <roy.reateguia@gmail.com>
2023-04-29 16:20:21 -0500

    finished mobile navigation bar

--- js/main.js
+++ js/main.js
@@ -1,9 +1,33 @@
-const h2 = document.getElementById('title')
+/* ===== Components ==== */
+const body = document.getElementById('body')
+const menuNav = document.getElementById('nav__Menu')
 
-const user = {
-    name: 'paty',
-    edad: 15,
-    email: "paty@gmail.com"
+
+/* ===== ICONS =====  */
+const iconMultiBox = document.getElementById('icon--multibox')
+const iconMoon = document.getElementById('icon--moon')
+const iconSun = document.getElementById('icon--sun')
+const iconClose = document.getElementById('icon--close')
+
+/* ======== FUNCIONES ======= */
+const toggleShowMenu = () => {
+    iconMultiBox.classList.toggle('hidden')
+    iconClose.classList.toggle('show')
+    menuNav.classList.toggle('menu--show')
 }
 
-console.log(puser);
+const toggleTheme = () => {
+    body.classList.toggle('darkTheme')
+    iconSun.classList.toggle('show')
+    iconMoon.classList.toggle('hidden')
+}
+
+
+/*========= EVENTOS ======== */
+
+iconMultiBox.addEventListener('click', toggleShowMenu )
+iconClose.addEventListener('click', toggleShowMenu )
+
+
+iconMoon.addEventListener('click', toggleTheme )
+iconSun.addEventListener('click', toggleTheme )
